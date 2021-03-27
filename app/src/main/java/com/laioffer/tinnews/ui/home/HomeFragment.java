c91dfadf90d327fc5d6b877d4461c5b8b3a0e39b
xren935 <59517115+xren935@users.noreply.github.com>
2021-03-27 12:48:22 -0700

    homepage in progress 🏡

--- app/src/main/java/com/laioffer/tinnews/ui/home/HomeFragment.java
+++ app/src/main/java/com/laioffer/tinnews/ui/home/HomeFragment.java
@@ -13,11 +13,20 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.laioffer.tinnews.R;
+import com.laioffer.tinnews.databinding.FragmentHomeBinding;
+import com.laioffer.tinnews.model.Article;
 import com.laioffer.tinnews.repository.NewsRepository;
 import com.laioffer.tinnews.repository.NewsViewModelFactory;
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
+import com.yuyakaido.android.cardstackview.StackFrom;
+
+import java.util.List;
 
 public class HomeFragment extends Fragment {
     private HomeViewModel viewModel;
+    private FragmentHomeBinding binding;
+    private CardStackLayoutManager layoutManager;
+    private List<Article> articles; // stores the fetched data
 
     public HomeFragment() {
         // Required empty public constructor
@@ -28,12 +37,21 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         // generate layout according to the xml
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        // return inflater.inflate(R.layout.fragment_home, container, false);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        // set up cardstack view
+        CardSwipeAdapter swipeAdapter = new CardSwipeAdapter();
+        layoutManager = new CardStackLayoutManager(requireContext());
+        layoutManager.setStackFrom(StackFrom.Top);
+        binding.homeCardStackView.setLayoutManager(layoutManager);
+        binding.homeCardStackView.setAdapter(swipeAdapter);
 
         NewsRepository repository = new NewsRepository();
         // ViewModelProvider cache old data so we don't new to create new ones that are already there
@@ -49,7 +67,9 @@ public class HomeFragment extends Fragment {
                         getViewLifecycleOwner(),
                         newsResponse -> {
                             if (newsResponse != null) {
-                                Log.d("HomeFragment", newsResponse.toString());
+                                // Log.d("HomeFragment", newsResponse.toString());
+                                articles = newsResponse.articles;
+                                swipeAdapter.setArticles(articles);
                             }
                         });
 
