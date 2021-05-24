2b21bf77f0ee5ea9c87aca49099ffcf3ac7b5a1b
xren935 <59517115+xren935@users.noreply.github.com>
2021-05-24 11:34:05 -0700

    save+deletepage done✅

--- app/src/main/java/com/laioffer/tinnews/ui/save/SaveFragment.java
+++ app/src/main/java/com/laioffer/tinnews/ui/save/SaveFragment.java
@@ -2,13 +2,22 @@ package com.laioffer.tinnews.ui.save;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.laioffer.tinnews.R;
+import com.laioffer.tinnews.databinding.FragmentSaveBinding;
+import com.laioffer.tinnews.model.Article;
+import com.laioffer.tinnews.repository.NewsRepository;
+import com.laioffer.tinnews.repository.NewsViewModelFactory;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,8 +35,48 @@ public class SaveFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    private FragmentSaveBinding binding;
+    private SaveViewModel viewModel;
+
     public SaveFragment() {
         // Required empty public constructor
+    }
+
+    // override the onViewCreated method to query saved articles
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        SavedNewsAdapter savedNewsAdapter = new SavedNewsAdapter();
+        binding.newsSavedRecyclerView.setAdapter(savedNewsAdapter);
+        binding.newsSavedRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+
+        NewsRepository repository = new NewsRepository(getContext());
+        viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository)).get(SaveViewModel.class);
+        viewModel
+                .getAllSavedArticles()
+                .observe(
+                        getViewLifecycleOwner(),
+                        savedArticles -> {
+                            if (savedArticles != null) {
+                                Log.d("SaveFragment", savedArticles.toString());
+                                savedNewsAdapter.setArticles(savedArticles);
+                            }
+                        });
+
+        // we call viewModel.deleteSavedArticle when onRemoveFavorite happens.
+        savedNewsAdapter.setItemCallback(new SavedNewsAdapter.ItemCallback() {
+            @Override
+            public void onOpenDetails(Article article) {
+                // TODO
+                Log.d("onOpenDetails", article.toString());
+            }
+
+            @Override
+            public void onRemoveFavorite(Article article) {
+                viewModel.deleteSavedArticle(article);
+            }
+        });
     }
 
     /**
@@ -61,6 +110,19 @@ public class SaveFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_save, container, false);
+        // return inflater.inflate(R.layout.fragment_save, container, false);
+        binding =  FragmentSaveBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
+
+//    @Override
+//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//
+//        SavedNewsAdapter savedNewsAdapter = new SavedNewsAdapter();
+//        binding.newsSavedRecyclerView.setAdapter(savedNewsAdapter);
+//        binding.newsSavedRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+//
+//        // ... existing code. Do not replace or remove.
+//    }
 }
